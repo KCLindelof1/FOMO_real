49d30527c65bb72256f7e7f915998fd7cd95b315
KCLindelof1 <kclindelof@gmail.com>
2018-01-30 11:29:51 -0700

    created the User class

--- account/models.py
+++ account/models.py
@@ -1,5 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from cuser.models import AbstractCUser
 
 # Create your models here.
-
+class User(AbstractCUser):
+    birthdate = models.DateTimeField(null=True)
+    address = models.TextField(null=True, blank=True)
+    city = models.TextField(null=True, blank=True)
+    state = models.TextField(null=True, blank=True)
+    zipcode = models.TextField(null=True, blank=True)
