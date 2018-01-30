1f9147509008e94ab68778aeda1930b5c6b2666f
KCLindelof1 <kclindelof@gmail.com>
2018-01-30 11:55:22 -0700

    finished 1-29 video, need help with shell error

--- account/models.py
+++ account/models.py
@@ -8,3 +8,6 @@ class User(AbstractCUser):
     city = models.TextField(null=True, blank=True)
     state = models.TextField(null=True, blank=True)
     zipcode = models.TextField(null=True, blank=True)
+
+    def get_purchases(self):
+        return [ 'Roku Ultimate 4', 'Skis', 'Computer']
