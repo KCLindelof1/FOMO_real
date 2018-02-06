d7834e5fec98b9fec8d452772dd611de5b6c1c1c
KCLindelof1 <kclindelof@gmail.com>
2018-02-06 09:18:43 -0700

    Finished class stuff. Reveiwed Forms and Exceptions. Check out formlib

--- account/views/testform.py
+++ account/views/testform.py
@@ -9,6 +9,9 @@ def process_request(request):
     if request.method == 'POST':
         form = TestForm(request.POST)
         if form.is_valid():
+            # once you get here, everything is clean. Don't do more data changes
+            # you can no longer inform the user that we have a problem
+            print(form.cleaned_data)
             # do the work of the form (ex: make payment, create the user
             return HttpResponseRedirect('/')
 
@@ -25,3 +28,25 @@ def process_request(request):
 class TestForm(forms.Form):
     favorite_ice_cream = forms.CharField(label='Favorite Ice Cream')
     renewal_date = forms.DateField(label="Renewal", help_text="Enter a date between now and 4 weeks (default 3).")
+    age = forms.IntegerField(label="Age")
+    password = forms.PasswordInput()
+    password2 = forms.PasswordInput()
+
+
+    # All our logic to mess with input data should be here:
+    def clean_age(self):
+        age = self.cleaned_data.get('age')          # already an INT, input came in as a string but then flipped to int by browser
+        if age < 18:
+            raise forms.ValidationError('You are not 18, no soup for you')
+            # don't allow the signup
+        return age + 1000
+
+    def clean_renewal_date(self):
+        return
+
+    def clean(self):
+        p1 = self.cleaned_data.get('password')
+        p2 = self.cleaned_data.get('password2')
+        if p1 != p2:
+            raise forms.ValidationError('Please ensure the passwords match.')
+        return self.cleaned_data
