6d207b1fa05a2288e5a1d618a3388d5a27ec22e2
KCLindelof1 <kclindelof@gmail.com>
2018-02-12 21:06:17 -0700

    accidentally made some db changes in master, switch over to a branch

--- fomo/settings.py
+++ fomo/settings.py
@@ -150,10 +150,22 @@ WSGI_APPLICATION = 'fomo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# Here is the new database that needst
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
