4babada73b18014b80d16eeb0181302118766cfb
KCLindelof1 <kclindelof@gmail.com>
2018-02-21 09:42:19 -0700

    not sure what i did last, picking it back up after sick week

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Product():
+    product_name = models.TextField(null=True, blank=True)
+    product_price = models.TextField(null=True, blank=True)
+    product_desc = models.TextField(null=True, blank=True)
+
+
+class Category():
+    cat_name = models.TextField(null=True, blank=True)
+    cat_desc = models.TextField(null=True, blank=True)
