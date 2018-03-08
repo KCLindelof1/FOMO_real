ccc98209a5f71aea3defb84a6c620203f7903781
KCLindelof1 <kclindelof@gmail.com>
2018-03-08 08:43:53 -0700

    added index.products.html

--- catalog/scripts/index.js
+++ catalog/scripts/index.js
@@ -1,6 +1,15 @@
-(function(context) {
-    
-    // utc_epoch comes from index.py
-    console.log('Current epoch in UTC is ' + context.utc_epoch);
-    
-})(DMP_CONTEXT.get());
+// Default javascript
+//
+// (function(context) {
+//
+//     // utc_epoch comes from index.py
+//     console.log('Current epoch in UTC is ' + context.utc_epoch);
+//
+// })(DMP_CONTEXT.get());
+
+
+$(function(context) {
+    return function() {
+        //code here
+    }
+}(DMP_CONTEXT.get()))
