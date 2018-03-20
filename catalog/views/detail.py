d1cf7cebc09cf7c521cb3da1551e004ac90d4c61
KCLindelof1 <kclindelof@gmail.com>
2018-03-19 22:44:38 -0600

    put categories on side in base_app.html

---
+++ catalog/views/detail.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponseRedirect, response
+from django_mako_plus import view_function, jscontext
+from datetime import datetime
+from catalog import models as cmod
+import math
+
+
+@view_function
+def process_request(request, pNum=1):
+    # something with cookies here
+    response.set_cookie()
+    request.get_cookie()
+
+    context = {
+    }
+    return request.dmp.render('detail.html', context)
